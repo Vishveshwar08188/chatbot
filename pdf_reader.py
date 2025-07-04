678bf99deb4916b986253c1ed151cca2484decd7
Vishveshwar08188 <jvishveshwar31@gmail.com>
2025-07-04 10:23:11 +0530

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,11 +1,10 @@
 import PyPDF2
 
-def load_pdf_text(pdf_path):
-    with open(pdf_path, "rb") as f:
-        reader = PyPDF2.PdfReader(f)
-        text = ""
-        for page in reader.pages:
-            content = page.extract_text()
-            if content:
-                text += content
-        return text
+def load_pdf_text(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        content = page.extract_text()
+        if content:
+            text += content
+    return text
